44bc83b23468ea349c14d5b5bc5cf1da33b2535b
Dmitrii Sviridov <dm@svrdv.ru>
2024-05-27 13:42:20 +0300

    init

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters.state import StateFilter
 from UserStates import UserStates
+from keyboard_helper import get_keyboard
 
 from config import TOKEN
 
@@ -16,7 +17,8 @@ dp = Dispatcher(storage=storage)
 
 @dp.message(Command("start"))
 async def start(message: types.Message, state: FSMContext):
-    await message.answer("Привет! Я пари-бот")
+    kb = get_keyboard(["/test"])
+    await message.answer("Привет! Я пари-бот", reply_markup=kb)
     await state.set_state(UserStates.BASE)
 
 
