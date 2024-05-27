84c5b9fd048cff07e8f97e8d299b893f981af552
Dmitrii Sviridov <dm@svrdv.ru>
2024-05-27 11:52:02 +0300

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import asyncio
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Привет! Я эхо-бот")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
