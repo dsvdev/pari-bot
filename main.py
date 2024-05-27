a34d96d30d31756feccc09630ee031ccd683f780
Dmitrii Sviridov <dm@svrdv.ru>
2024-05-27 13:34:58 +0300

    init

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import asyncio
+
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.state import StateFilter
+from UserStates import UserStates
+
 from config import TOKEN
 
+storage = MemoryStorage()
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
+
 
 @dp.message(Command("start"))
+async def start(message: types.Message, state: FSMContext):
+    await message.answer("Привет! Я пари-бот")
+    await state.set_state(UserStates.BASE)
+
+
+@dp.message(Command("test"), StateFilter(UserStates.BASE))
 async def start(message: types.Message):
-    await message.answer("Привет! Я эхо-бот")
+    await message.answer("Ты в состоянии BASE")
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
